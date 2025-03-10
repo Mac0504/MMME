ce78ca877c942f598d9a7339ce177d5f64e0a3af
Mac0504 <43312227+Mac0504@users.noreply.github.com>
2025-03-10 11:15:12 +0800

    Add files via upload

---
+++ codes/data/EEG/cdt2mat.m
@@ -0,0 +1,3 @@
+
+EEG = loadcurry('E:\10.Doctoral materials\0.Micro-expression\1.PROJECTS\0.My_project\Multimodal ME\Multi-phy\wangxiaomin20230718\EEG\Acquisition 01.cdt', 'CurryLocations', 'False');
+save('eeg.mat', 'EEG')
